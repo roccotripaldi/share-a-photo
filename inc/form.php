a342618c9fdf16869ef1e6d8d149051ac1d3503d
Rocco Tripaldi <tripaldir@gmail.com>
2015-04-12 19:58:47 -0400

    wake up javascript

--- inc/form.php
+++ inc/form.php
@@ -2,11 +2,21 @@
 
 $defaults = array(
 	'open' => 'Share A Photo',
+	'id' => 'shaph-form-' . wp_generate_password( 6, false ),
 );
 
 $settings = array_merge( $defaults, $this->settings );
 
 ?>
-<form id="shaph-form">
-	<input type="button" value="<?php echo $settings['open']; ?>" />
+<form class="shaph-form" id="<?php echo $settings['id']; ?>">
+	<input
+		type="button"
+		value="<?php echo $settings['open']; ?>"
+		class="shaph-button-open"
+	    data-id="<?php echo $settings['id']; ?>"
+	/>
+
+	<fieldset>
+	</fieldset>
+
 </form>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+jQuery( function() {
 
+	jQuery(".shaph-button-open").click( function() {
+		var id = jQuery(this).data("id");
+		jQuery("#" + id).addClass("open");
+		console.log('click');
+	});
+
+} );
