ead428f846e50740dd94fb368f84bdcd5b88b38f
Rocco Tripaldi <tripaldir@gmail.com>
2015-04-13 09:19:01 -0400

    started on base css and base js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 jQuery( function() {
 
-	jQuery(".shaph-button").click( function() {
-		console.log('click');
-	});
+	jQuery(".shaph-button").click( shareAPhoto.open );
+	jQuery("#shaph-cancel").click( shareAPhoto.close );
 
 } );
+
+var shareAPhoto = {
+	open: function() {
+		jQuery("#shaph-form").addClass("open");
+	},
+	close: function() {
+		jQuery("#shaph-form").removeClass("open");
+	}
+};
