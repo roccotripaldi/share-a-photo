ea2275cf08d4d75451580d7963fba36c6a3d3794
Rocco Tripaldi <tripaldir@gmail.com>
2015-09-07 20:27:44 -0400

    Basic styles for the captions page

--- inc/caption.php
+++ inc/caption.php
@@ -1,5 +1,23 @@
 <script id="shaph-template-caption" type="text/template">
+	<h3 class="shaph-title">
+		<% if ( files.length > 1 ) { %>
+		Tell us about your photos
+		<% } else { %>
+		Tell us about your photo
+		<% } %>
+	</h3>
 	<% for( var file in files ) { %>
-		<p></p><img src="<%= files[file].url %>" width="50" /></p>
+		<div class="shaph-uploaded-image">
+			<img src="<%= files[file].url %>" width="80" />
+			<div class="shaph-image-attributes">
+				<label>Title</label>
+				<input class="text-input" type="text" name="fileMeta[<%= file %>][title]" />
+				<label>Caption</label>
+				<textarea class="text-input" name="fileMeta[<%= file %>][caption]"></textarea>
+			</div>
+		</div>
 	<% } %>
+	<div class="shaph-footer-buttons">
+		<input type="button" id="shaph-finish" value="finish" />
+	</div>
 </script>
--- inc/uploader.php
+++ inc/uploader.php
@@ -10,6 +10,9 @@ $uploader_labels = apply_filters( 'shaph_uploader_labels', $uploader_labels );
 <script id="shaph-template-uploader" type="text/template">
 	<h3 class="shaph-title"><?php echo esc_attr( $uploader_labels['title'] ); ?></h3>
 	<ul id="shaph-filelist"></ul>
-	<input type="button" id="shaph-browse" value="<?php echo esc_attr( $uploader_labels['browse'] ); ?>" />
-	<input type="button" id="shaph-start-upload" value="<?php echo esc_attr( $uploader_labels['upload'] ); ?>" />
+	<div class="shaph-footer-buttons">
+		<input type="button" id="shaph-browse" value="<?php echo esc_attr( $uploader_labels['browse'] ); ?>" />
+		<input type="button" id="shaph-start-upload" value="<?php echo esc_attr( $uploader_labels['upload'] ); ?>" />
+	</div>
+
 </script>
