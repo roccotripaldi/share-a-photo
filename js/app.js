b09f6f395874329de4988d8e0b640a5b3d94b3ff
Rocco Tripaldi <tripaldir@gmail.com>
2015-09-19 22:40:41 -0400

    Change the cancel button on open / finish.

--- js/app.js
+++ js/app.js
@@ -170,6 +170,7 @@ var shareAPhotoApp = Backbone.Model.extend( {
 		jQuery( '#shaph' ).addClass( 'open' );
 		shareAPhoto.App.renderTemplate( '#shaph-page', 'uploader' );
 		shareAPhoto.App.renderButtons();
+		jQuery( '#shaph-cancel' ).attr( 'value', 'Cancel' );
 		shareAPhoto.App.initializePluploader();
 	},
 
