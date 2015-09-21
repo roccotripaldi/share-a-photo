0bddffaff8cd37d960c1d313618ec159aa95cc6e
Rocco Tripaldi <tripaldir@gmail.com>
2015-09-21 09:15:41 -0400

    Disable action buttons until upload is complete.

--- inc/template.footer-buttons.php
+++ inc/template.footer-buttons.php
@@ -7,7 +7,7 @@
         <% if ( numExtensions > 0 ) { %>
             <input type="button" id="shaph-next" value="Next" />
         <% } else { %>
-            <input type="button" id="shaph-finish" value="Finish" <% if ( isUploading ) { %>disable="disabled"<% } %> />
+            <input type="button" id="shaph-finish" value="Finish" <% if ( isUploading === true ) { %>disabled="disabled"<% } %> />
         <% } %>
     <% } else { %>
         <input type="button" id="shaph-next-image" value="Next Image" />
@@ -15,7 +15,7 @@
 <% } else if ( currentExtensionIndex !== false ) { %>
     <input type="button" id="shaph-previous" value="Previous" />
     <% if ( currentExtensionIndex === numExtensions - 1 ) { %>
-        <input type="button" id="shaph-finish" value="Finish" <% if ( isUploading ) { %>disable="disabled"<% } %> />
+        <input type="button" id="shaph-finish" value="Finish" <% if ( isUploading === true ) { %>disabled="disabled"<% } %> />
     <% } else { %>
         <input type="button" id="shaph-next" value="Next" />
     <% } %>
